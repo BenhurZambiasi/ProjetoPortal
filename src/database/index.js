70ce03d26747ba1ac3cf4e6b33e3ca8001452b23
Carlos Martins <c.e.martins.nunes.91@gmail.com>
2020-10-22 14:01:41 -0300

    teste

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0.qtbrz.mongodb.net/mongoportal?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-})
+mongoose
+  .connect(
+    'mongodb+srv://admin:admin@cluster0.qtbrz.mongodb.net/mongoportal?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    },
+  )
   .then(() => console.log('MongoDb connected!'))
   .catch((err) => console.log(err));
 
