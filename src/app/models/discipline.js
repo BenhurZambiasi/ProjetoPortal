8ccb8013cbfe8b49445ff70d433b515f09dbd51b
Carlos Martins <c.e.martins.nunes.91@gmail.com>
2020-10-23 09:35:52 -0300

    atualizando model discipline

--- src/app/models/discipline.js
+++ src/app/models/discipline.js
@@ -3,23 +3,28 @@ import mongoose from '../../database';
 const DisciplineSchema = new mongoose.Schema({
   idTeacher: {
     type: String,
+    required: true,
   },
   content: [
     {
       name: {
         type: String,
+        required: true,
       },
       title: {
         type: String,
+        required: true,
       },
       description: {
         type: String,
+        required: true,
       },
     },
   ],
 
   numberStudents: {
     type: Number,
+    required: true,
   },
 });
 
