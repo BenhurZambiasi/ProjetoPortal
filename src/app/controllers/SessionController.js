f68d07f5825c3ab1357ae37b76a0c35ca1d494e9
Benhur <benhur_zambiasi@hotmail.com>
2020-10-29 14:10:52 -0300

    separando login de adm dos demais

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -11,6 +11,23 @@ class SessionController {
       const user = await User.findOne({ email }).select('+password');
       if (!user)
         return res.status(400).send({ error: 'Email inválido ' })
+      if (user.password !== password)
+        return res.status(400).send({ error: 'Senha inválida' })
+      const token = jwt.sign({ id: user.id }, authConfig.secret, {
+        expiresIn: '7d'
+      })
+      res.send({ user, token })
+    } catch (error) {
+      return res.status(400).send({ error: 'Falha no login, usuário ou senha inválida' })
+    }
+  }
+
+  async loginAdm(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email }).select('+password');
+      if (!user)
+        return res.status(400).send({ error: 'Email inválido ' })
       if (user.usertype != 1)
         return res.status(400).send({ error: 'Acesso negado, é necessario ser adminstrador' })
       if (user.password !== password)
