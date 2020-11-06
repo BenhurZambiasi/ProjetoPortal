8bca7539de0dc8cb8f2e6fb28b7f5a66ec73d0c7
Benhur <benhur_zambiasi@hotmail.com>
2020-11-06 14:40:49 -0300

    finalizando

--- src/app/controllers/NotaController.js
+++ src/app/controllers/NotaController.js
@@ -3,23 +3,22 @@ import Discipline from '../models/discipline';
 class NotaController {
   //criando
   async store(req, res) {
-    const { idDisciplina } = req.params;
-    const { notas } = req.body
-    const disciplines = await Discipline.findByIdAndUpdate({ _id: idDisciplina }, { $push: { notas } }, { new: true });
-    return res.send({ disciplines })
+    try {
+      const { idDisciplina } = req.params;
+      const { notas } = req.body
+      const disciplines = await Discipline.findByIdAndUpdate({ _id: idDisciplina }, { $push: { notas } }, { new: true });
+      return res.send({ disciplines })
+    } catch (error) {
+      return res.status(400).send({ error: 'Preencha todos os campos' })
+    }
   }
 
-
-
-
-
-  
 
   async index(req, res) {
     const disciplines = await Discipline.find();
     var result = [];
     disciplines.forEach(discipline => {
-      result.push({ discipline: discipline.name, nota: discipline.notas});
+      result.push({ discipline: discipline.name, nota: discipline.notas });
     });
 
     return res.send({ result })
